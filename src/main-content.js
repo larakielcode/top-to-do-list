09368e2ad21f5a96935c7a172efdc114eac96cdf
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-07-01 01:43:35 +0800

    added some code to the initial design

--- src/main-content.js
+++ src/main-content.js
@@ -1,9 +1,22 @@
 // main-content.js
 
-export default function displayUi() {
-    const topNavWrapper = document.querySelector('#top-nav');
-    const testpara = document.createElement('p');
+import groupsvg from '../asset/icons/group.svg';
 
-    topNavWrapper.appendChild(testpara);
-    testpara.textContent = 'this works';
+export default function displayUi() {
+    // code for the navbar
+    const navMainSection = document.querySelector('nav');
+    const navWrapper = document.createElement('div');
+    const navProjectContainer = document.createElement('div');
+    const navTodoOptionsContainer = document.createElement('div');
+
+    navMainSection.appendChild(navWrapper);
+    navWrapper.classList.add('nav-wrapper');
+
+    navWrapper.appendChild(navProjectContainer);
+    navProjectContainer.classList.add('nav-project-container');
+    const navProjectFirstDiv = document.createElement('div');
+
+
+    navWrapper.appendChild(navTodoOptionsContainer);
+    navTodoOptionsContainer.classList.add('nav-todo-options');
 }
